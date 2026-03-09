39d458239c58f2b60cd249028c31897215da8b16
Gabriel Valentoni <gabriel@lambdatt.com.br>
2026-03-09 01:12:05 -0300

    refactor: Standardize code formatting, update service calls to use `$getService("toolcase/http")`, and improve readability in notification components.

--- src/components/PushNotification.vue
+++ src/components/PushNotification.vue
@@ -7,7 +7,13 @@
 
       <q-card-actions align="right">
         <q-btn flat label="Fechar" color="grey-8" v-close-popup />
-        <q-btn flat label="Habilitar" color="primary" @click="enable()" v-close-popup />
+        <q-btn
+          flat
+          label="Habilitar"
+          color="primary"
+          @click="enable()"
+          v-close-popup
+        />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -15,11 +21,11 @@
 
 <script>
 export default {
-  name: 'component-PushNotification',
+  name: "component-PushNotification",
 
   data() {
     return {
-      showDialog: false
+      showDialog: false,
     };
   },
 
@@ -27,8 +33,8 @@ export default {
     async enable() {
       const permission = await Notification.requestPermission();
 
-      if (permission === 'granted') {
-        if (!localStorage.getItem('FCMPushToken')) {
+      if (permission === "granted") {
+        if (!localStorage.getItem("FCMPushToken")) {
           await this.createToken();
         } else {
           await this.tokenRecycle();
@@ -40,31 +46,36 @@ export default {
 
     async createToken() {
       // Handle device key
-      const deviceKey = await this.$getService('iam/device').getDeviceKey();
+      const deviceKey = await this.$getService("iam/device").getDeviceKey();
 
       try {
-        const token = await this.$getService('messaging/firebase').getToken();
-        await this.$http
-          .setHeader('Iam-Device-Key', deviceKey)
-          .post('/api/messaging/v1/push/subscription', { token: token });
+        const token = await this.$getService("messaging/firebase").getToken();
+        await this.$getService("toolcase/http")
+          .setHeader("Iam-Device-Key", deviceKey)
+          .post("/api/messaging/v1/push/subscription", { token: token });
 
-        localStorage.setItem('FCMPushToken', token);
+        localStorage.setItem("FCMPushToken", token);
       } catch (error) {
-        console.error('Error creating push subscription.', error);
+        console.error("Error creating push subscription.", error);
       }
 
       return;
     },
 
     async tokenRecycle() {
-      const currentToken = localStorage.getItem('FCMPushToken');
-      const validToken = await this.$getService('messaging/firebase').getToken();
+      const currentToken = localStorage.getItem("FCMPushToken");
+      const validToken = await this.$getService(
+        "messaging/firebase",
+      ).getToken();
       if (!!currentToken && currentToken !== validToken) {
         try {
-          await this.$http.put(`/api/messaging/v1/push/subscription/${currentToken}`, { newToken: validToken });
-          localStorage.setItem('FCMPushToken', validToken);
+          await this.$getService("toolcase/http").put(
+            `/api/messaging/v1/push/subscription/${currentToken}`,
+            { newToken: validToken },
+          );
+          localStorage.setItem("FCMPushToken", validToken);
         } catch (error) {
-          console.error('Error recycling push subscription.', error);
+          console.error("Error recycling push subscription.", error);
         }
       }
 
@@ -73,17 +84,18 @@ export default {
 
     init() {
       // Check if the browser supports notifications and service workers
-      if (!("Notification" in window) || !("serviceWorker" in navigator)) return;
+      if (!("Notification" in window) || !("serviceWorker" in navigator))
+        return;
 
       // Check if the user already denied exlplicitly the notification permission:
       if (Notification.permission === "denied") return;
 
-      this.showDialog = (Notification.permission !== "granted");
+      this.showDialog = Notification.permission !== "granted";
     },
   },
 
   mounted() {
     this.init();
-  }
-}
+  },
+};
 </script>
--- src/pages/notifications.vue
+++ src/pages/notifications.vue
@@ -2,26 +2,63 @@
   <Page PageTitle="Minhas Notificações" :Breadcrumb="breadcrumb">
     <div ref="scrollTargetRef">
       <q-infinite-scroll @load="fetchNotifications" :offset="250">
-        <div v-if="notifications.length > 0" class="text-grey-8 text-justify q-gutter-y-md">
+        <div
+          v-if="notifications.length > 0"
+          class="text-grey-8 text-justify q-gutter-y-md"
+        >
           <div v-for="(notification, index) in notifications" :key="index">
-            <q-card class="round-sm border-left-5-solid q-pa-md" clickable @click="openNotification(notification)"
-              :style="`border-color: ${notification.important == 'Y' ? '#F2C037' : 'transparent'}`">
+            <q-card
+              class="round-sm border-left-5-solid q-pa-md"
+              clickable
+              @click="openNotification(notification)"
+              :style="`border-color: ${
+                notification.important == 'Y' ? '#F2C037' : 'transparent'
+              }`"
+            >
               <!-- Header -->
               <div class="row flex-center">
-                <div class="col-auto q-mr-sm" style="display: flex; align-items: center; justify-content: center;">
-                  <q-img :src="notification.authorImg" style="border-radius: 50%; height: 30px; width: 30px;" />
+                <div
+                  class="col-auto q-mr-sm"
+                  style="
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                  "
+                >
+                  <q-img
+                    :src="notification.authorImg"
+                    style="border-radius: 50%; height: 30px; width: 30px"
+                  />
                 </div>
-                <div class="col-auto text-bold q-pr-sm">{{ notification.author }}</div>
-                <q-badge class="col-auto" v-if="notification.read == 'N'" rounded color="red">novo</q-badge>
-                <div class="col text-caption text-right text-grey-6">{{ notification.date }}</div>
+                <div class="col-auto text-bold q-pr-sm">
+                  {{ notification.author }}
+                </div>
+                <q-badge
+                  class="col-auto"
+                  v-if="notification.read == 'N'"
+                  rounded
+                  color="red"
+                  >novo</q-badge
+                >
+                <div class="col text-caption text-right text-grey-6">
+                  {{ notification.date }}
+                </div>
               </div>
               <!-- Conteúdo -->
               <div class="q-pt-sm">
                 <div class="row items-center">
-                  <q-icon v-if="notification.important === 'Y'" size="12px" class="text-warning q-mr-sm"
-                    name="fas fa-star" style="padding-bottom: 2px;">
+                  <q-icon
+                    v-if="notification.important === 'Y'"
+                    size="12px"
+                    class="text-warning q-mr-sm"
+                    name="fas fa-star"
+                    style="padding-bottom: 2px"
+                  >
                   </q-icon>
-                  <div class="text-bold" style="font-size: 0.9rem; word-break: break-word;">
+                  <div
+                    class="text-bold"
+                    style="font-size: 0.9rem; word-break: break-word"
+                  >
                     {{ notification.title }}
                   </div>
                 </div>
@@ -33,14 +70,23 @@
         <div v-else>
           <div class="column q-pa-md flex-center text-center">
             <div class="q-mb-sm">
-              <q-icon name="fas fa-bell" size="60px" color="grey-6"><q-badge floating
-                  color="grey-8">0</q-badge></q-icon>
+              <q-icon name="fas fa-bell" size="60px" color="grey-6"
+                ><q-badge floating color="grey-8">0</q-badge></q-icon
+              >
             </div>
-            <div class="text-bold q-mb-sm" style="font-size: 18px;">Nenhuma notificação encontrada!</div>
-            <div>Assim que receber sua primeira notificação,<br>ela aparecerá aqui nesta página</div>
+            <div class="text-bold q-mb-sm" style="font-size: 18px">
+              Nenhuma notificação encontrada!
+            </div>
+            <div>
+              Assim que receber sua primeira notificação,<br />ela aparecerá
+              aqui nesta página
+            </div>
           </div>
         </div>
-        <template v-if="!stopRequests && notifications.length > 0" v-slot:loading>
+        <template
+          v-if="!stopRequests && notifications.length > 0"
+          v-slot:loading
+        >
           <div class="row text center justify-center">
             <q-spinner-dots color="grey-8" size="4em" />
           </div>
@@ -50,21 +96,31 @@
 
     <!-- News Content Modal -->
     <q-dialog @hide="notificationContent = null" v-model="showModal" full-width>
-      <q-card class="cursor-pointer q-pa-md" style="border-radius: 10px;">
+      <q-card class="cursor-pointer q-pa-md" style="border-radius: 10px">
         <!-- Header -->
         <div class="row flex-center">
-          <div class="col-auto q-mr-sm" style="display: flex; align-items: center; justify-content: center;">
-            <q-img :src="notificationContent.authorImg" style="border-radius: 50%; height: 40px; width: 40px;" />
+          <div
+            class="col-auto q-mr-sm"
+            style="display: flex; align-items: center; justify-content: center"
+          >
+            <q-img
+              :src="notificationContent.authorImg"
+              style="border-radius: 50%; height: 40px; width: 40px"
+            />
           </div>
           <div class="col text-bold">{{ notificationContent.author }}</div>
-          <div class="col text-caption text-right text-grey-6">{{ notificationContent.date }}</div>
+          <div class="col text-caption text-right text-grey-6">
+            {{ notificationContent.date }}
+          </div>
           <!-- <q-btn class="col-auto q-ml-sm" rounded dense icon="close" v-close-popup style="font-size: 0.7rem;" /> -->
         </div>
 
         <!-- Conteúdo -->
         <div class="q-pt-md q-pb-sm">
           <div class="row items-center q-mb-sm">
-            <div class="text-bold" style="font-size: 0.93rem;"> {{ notificationContent.title }} </div>
+            <div class="text-bold" style="font-size: 0.93rem">
+              {{ notificationContent.title }}
+            </div>
           </div>
           <!-- <q-separator /> -->
           <div>
@@ -82,31 +138,31 @@
 </template>
 <script>
 // Services:
-import ENDPOINTS from '../ENDPOINTS'
+import ENDPOINTS from "../ENDPOINTS";
 
 export default {
-  name: 'lambdatt-ui-messaging-pages-notifications',
+  name: "lambdatt-ui-messaging-pages-notifications",
 
   data() {
     return {
       firstLoad: true,
-      notifications: [],          // Notificações na Tela
-      notificationContent: null,  // Conteúdo do Modal
+      notifications: [], // Notificações na Tela
+      notificationContent: null, // Conteúdo do Modal
       showModal: false,
       stopRequests: false,
 
       // Services:
-      $http: this.$getService('toolcase/http'),
-      $utils: this.$getService('toolcase/utils'),
-    }
+      $http: this.$getService("toolcase/http"),
+      $utils: this.$getService("toolcase/utils"),
+    };
   },
 
   computed: {
     breadcrumb() {
       return [
-        { label: 'Home', icon: "fas fa-home", to: "/" },
-        { label: 'Notificações', icon: "fas fa-bell" },
-      ]
+        { label: "Home", icon: "fas fa-home", to: "/" },
+        { label: "Notificações", icon: "fas fa-bell" },
+      ];
     },
   },
 
@@ -121,7 +177,7 @@ export default {
         { name: "dia", plural: "dias", seconds: 60 * 60 * 24 },
         { name: "hora", plural: "horas", seconds: 60 * 60 },
         { name: "minuto", plural: "minutos", seconds: 60 },
-        { name: "segundo", plural: "segundos", seconds: 1 }
+        { name: "segundo", plural: "segundos", seconds: 1 },
       ];
 
       for (let unit of units) {
@@ -137,92 +193,123 @@ export default {
     async getNotifications(params) {
       // API request
       try {
-        const response = await this.$http.get(ENDPOINTS.NOTIFICATIONS.NOTIFICATION, params);
+        const response = await this.$getService("toolcase/http").get(
+          ENDPOINTS.NOTIFICATIONS.NOTIFICATION,
+          params,
+        );
         if (response && response.data) {
           return response.data.map((notify) => ({
             date: this.timeAgo(notify.dt_created),
             author: notify.author_name || "Desconhecido",
-            authorImg: notify.author_avatar ? (notify.author_avatar == 'system' ? '/resources/img/system-icon.jpg' : notify.author_avatar) : '/resources/img/unknown-user.jpg',
+            authorImg: notify.author_avatar
+              ? notify.author_avatar == "system"
+                ? "/resources/img/system-icon.jpg"
+                : notify.author_avatar
+              : "/resources/img/unknown-user.jpg",
             title: notify.ds_headline,
             brief: notify.ds_brief,
             content: notify.tx_content,
             read: notify.do_read,
             important: notify.do_important,
-            ds_key: notify.ds_key
+            ds_key: notify.ds_key,
           }));
         }
       } catch (error) {
         if (error.response?.status == 401) {
-          this.$router.push('/login?goTo=/notifications');
+          this.$router.push("/login?goTo=/notifications");
         } else {
-          this.$utils.notifyError(error);
+          this.$getService("toolcase/utils").notifyError(error);
         }
-        console.error("An error occurred while attempting to retrieve the object's data.", error);
+        console.error(
+          "An error occurred while attempting to retrieve the object's data.",
+          error,
+        );
       }
     },
 
     async openNotification(item) {
       this.notificationContent = item;
       this.showModal = true;
-      if (item.read == 'N') { await this.markAsRead(item); }
+      if (item.read == "N") {
+        await this.markAsRead(item);
+      }
     },
 
     async markAsRead(item) {
       try {
         // API request
-        await this.$http.put(`${ENDPOINTS.NOTIFICATIONS.NOTIFICATION}/mark-as-read/${item.ds_key}`);
-        item.read = 'Y';
+        await this.$getService("toolcase/http").put(
+          `${ENDPOINTS.NOTIFICATIONS.NOTIFICATION}/mark-as-read/${item.ds_key}`,
+        );
+        item.read = "Y";
       } catch (error) {
-        this.$utils.notifyError(error);
-        console.error("An error occurred while attempting to update the object's data.", error);
+        this.$getService("toolcase/utils").notifyError(error);
+        console.error(
+          "An error occurred while attempting to update the object's data.",
+          error,
+        );
       }
     },
 
     async fetchNotifications(index, done) {
-      if (this.stopRequests) { return }
+      if (this.stopRequests) {
+        return;
+      }
 
       this.firstLoad = false;
 
       const payload = {
         $limit: 15,
         $page: index,
-        $sort_direction: 'DESC',
-        $sort_by: '2',
+        $sort_direction: "DESC",
+        $sort_by: "2",
         $limit_multiplier: 1,
-      }
+      };
 
       const notificationBatch = await this.getNotifications(payload);
 
-      if (notificationBatch.length === 0) { this.stopRequests = true; return }
+      if (notificationBatch.length === 0) {
+        this.stopRequests = true;
+        return;
+      }
       this.notifications = [...this.notifications, ...notificationBatch];
       done();
     },
 
     async remove(key) {
       // Confimation
-      if (!confirm('Deseja excluir as informações?')) { return false; }
+      if (!confirm("Deseja excluir as informações?")) {
+        return false;
+      }
 
       // Emitting the loading event
-      this.$emit('load', 'item-remove');
+      this.$emit("load", "item-remove");
 
       // API request
       try {
-        await this.$http.delete(ENDPOINTS.NOTIFICATIONS.NOTIFICATION + '/' + key);
-        this.$utils.notify({
-          message: 'Notificação excluída com sucesso',
-          type: 'positive',
-          position: 'top-right'
+        await this.$getService("toolcase/http").delete(
+          ENDPOINTS.NOTIFICATIONS.NOTIFICATION + "/" + key,
+        );
+        this.$getService("toolcase/utils").notify({
+          message: "Notificação excluída com sucesso",
+          type: "positive",
+          position: "top-right",
         });
-        this.notifications = this.notifications.filter(obj => obj.ds_key !== key);
+        this.notifications = this.notifications.filter(
+          (obj) => obj.ds_key !== key,
+        );
         this.showModal = false;
       } catch (error) {
-        this.$utils.notifyError(error);
-        console.error("An error occurred while attempting to delete the object.", error);
+        this.$getService("toolcase/utils").notifyError(error);
+        console.error(
+          "An error occurred while attempting to delete the object.",
+          error,
+        );
       } finally {
         // Finalizing the loading event
-        this.$emit('loaded', 'item-remove');
-      };
+        this.$emit("loaded", "item-remove");
+      }
     },
   },
-}
+};
 </script>
