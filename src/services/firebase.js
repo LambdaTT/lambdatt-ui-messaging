10e03bf93b13289f254434e305b26ba2ddd27de0
Gabriel Valentoni <gabriel@lambdatt.com.br>
2026-04-12 21:09:20 -0300

    refactor: update firebase service to use direct config import and apply consistent code formatting

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,36 +1,39 @@
-import { initializeApp } from 'firebase/app'
-import { getMessaging, getToken } from 'firebase/messaging'
-import $sys from 'src/lambdatt'
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken } from "firebase/messaging";
+import { getConfigs } from "src/configs";
 
-const configs = $sys.getConfigs()
+const configs = getConfigs("fcm");
 
 // Your web app's Firebase configuration
-const firebaseApp = initializeApp(configs.fcm.app)
+const firebaseApp = initializeApp(configs.app);
 
 // Initialize Firebase Cloud Messaging
-const messaging = getMessaging(firebaseApp)
-const vapidKey = configs.fcm.vapidKey
+const messaging = getMessaging(firebaseApp);
+const vapidKey = configs.vapidKey;
 
 export default {
   async getToken() {
     try {
-      let registration = await navigator.serviceWorker.getRegistration()
+      let registration = await navigator.serviceWorker.getRegistration();
       if (!registration) {
         const swFile =
-          typeof process !== 'undefined' && process.env.SERVICE_WORKER_FILE
+          typeof process !== "undefined" && process.env.SERVICE_WORKER_FILE
             ? process.env.SERVICE_WORKER_FILE
-            : '/sw.js'
-        await navigator.serviceWorker.register(swFile)
+            : "/sw.js";
+        await navigator.serviceWorker.register(swFile);
       }
-      registration = await navigator.serviceWorker.ready
+      registration = await navigator.serviceWorker.ready;
 
       return await getToken(messaging, {
         vapidKey: vapidKey,
         serviceWorkerRegistration: registration,
-      })
+      });
     } catch (error) {
-      console.error('Error retrieving Firebase Push Notification token.', error)
-      throw error
+      console.error(
+        "Error retrieving Firebase Push Notification token.",
+        error,
+      );
+      throw error;
     }
   },
-}
+};
